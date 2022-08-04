ab6fcf3d90134f8387e6f385148ed5f757383e79
Atin Gupta <atingupta@email.com>
2022-08-04 09:16:10 +0530

    -

--- README.md
+++
@@ -1,1 +0,0 @@
-# Python and R Programming
--- gitlab.bat
+++
@@ -1,7 +0,0 @@
-git checkout am1
-git pull gitlab-origin am1
-del .gitignore
-copy .gitignore-gitlab .gitignore
-git add *
-git commit -am "-"
-git push gitlab-origin am1
